be1e7c319bd5c4bc7d065365c5fd3e4a97d92e5c
slavP <slav.pilus@gmail.com>
2018-12-02 15:03:49 +0000

    day 2 part 2

---
+++ src/day2/day2-part2.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.Open("./src/day2/input.txt")
+	if err != nil {
+		log.Fatal("cannot open input file")
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	var words []string
+
+	for scanner.Scan() {
+		words = append(words, scanner.Text())
+	}
+
+	for _, element := range words {
+		for _, element2 := range words {
+			characterArray := strings.Split(element, "")
+			characterArray2 := strings.Split(element2, "")
+
+			diffCount := 0
+
+			for i, _ := range characterArray {
+				if characterArray[i] != characterArray2[i] {
+					diffCount++
+				}
+			}
+
+			if diffCount == 1 {
+				for i, _ := range characterArray {
+					if characterArray[i] == characterArray2[i] {
+						fmt.Print(characterArray[i])
+					}
+				}
+				os.Exit(0)
+			}
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+
+}
